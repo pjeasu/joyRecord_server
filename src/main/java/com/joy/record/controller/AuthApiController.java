904e283bb59863818ebe38e2c8e23deeae7968da
pjeasu <pjeasu@gmail.com>
2024-07-18 22:30:23 +0900

    context path 수정

--- src/main/java/com/joy/record/controller/AuthApiController.java
+++ src/main/java/com/joy/record/controller/AuthApiController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthApiController {
 
     private final AuthService authService;
